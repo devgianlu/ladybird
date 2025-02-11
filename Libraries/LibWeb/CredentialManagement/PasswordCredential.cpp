981d32327afd0a964613eb2edd35ee459e661cb0
devgianlu <altomanigianluca@gmail.com>
2025-02-11 14:56:50 +0100

    LibWeb: Implement `PasswordCredential` internal `Create` method

--- Libraries/LibWeb/CredentialManagement/PasswordCredential.cpp
+++ Libraries/LibWeb/CredentialManagement/PasswordCredential.cpp
@@ -4,8 +4,12 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <LibWeb/Bindings/ExceptionOrUtils.h>
+#include <LibWeb/CredentialManagement/CredentialsContainer.h>
 #include <LibWeb/CredentialManagement/PasswordCredential.h>
 #include <LibWeb/CredentialManagement/PasswordCredentialOperations.h>
+#include <LibWeb/HTML/AutocompleteElement.h>
+#include <LibWeb/XHR/FormData.h>
 
 namespace Web::CredentialManagement {
 
@@ -49,6 +53,33 @@ void PasswordCredential::initialize(JS::Realm& realm)
 {
     WEB_SET_PROTOTYPE_FOR_INTERFACE(PasswordCredential);
     Base::initialize(realm);
+}
+
+// https://w3c.github.io/webappsec-credential-management/#create-passwordcredential
+JS::ThrowCompletionOr<Variant<Empty, GC::Ref<Credential>, GC::Ref<CreateCredentialAlgorithm>>> PasswordCredentialInterface::create(JS::Realm& realm, URL::Origin const& origin, CredentialCreationOptions const& options, bool) const
+{
+    // 1. Assert: options["password"] exists, and sameOriginWithAncestors is unused.
+    VERIFY(options.password.has_value());
+
+    auto maybe_result = options.password->visit(
+        // 2. If options["password"] is an HTMLFormElement, return the result of executing Create a PasswordCredential
+        //    from an HTMLFormElement given options["password"] and origin. Rethrow any exceptions.
+        [&](GC::Root<HTML::HTMLFormElement> const& form) {
+            return create_password_credential(realm, *form, origin);
+        },
+        // 3. If options["password"] is a PasswordCredentialData, return the result of executing
+        //     Create a PasswordCredential from PasswordCredentialData given options["password"]. Rethrow any exceptions.
+        [&](PasswordCredentialData const& data) {
+            return create_password_credential(realm, data, origin);
+        },
+        // 4. Throw a TypeError exception.
+        [&](auto) {
+            return realm.vm().throw_completion<JS::TypeError>("options.password must be an HTMLFormElement or a PasswordCredentialData"sv);
+        });
+    if (maybe_result.is_error())
+        return Bindings::exception_to_throw_completion(realm.vm(), maybe_result.release_error());
+
+    return maybe_result.release_value();
 }
 
 }
